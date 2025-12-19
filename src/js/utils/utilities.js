724fa572a8fc0dc015624f60d3d45468986744aa
Almee Christian <almee@Almees-MacBook-Pro.local>
2025-12-19 16:08:54 -0500

    removed unnecessary changes.

--- src/js/utils/utilities.js
+++ src/js/utils/utilities.js
@@ -38,8 +38,7 @@ export const orderListByFurthestFutureStartDate = (incomingObjectList) => {
 
 export const sortByNoTeamFirst = (incomingObjectList, allPeopleTeamIdLists = {}) => {
   if (!incomingObjectList) return [];
-  const safeAllPeopleTeamIdLists = allPeopleTeamIdLists || {};
-  const personIdsInTeams = new Set(Object.keys(safeAllPeopleTeamIdLists));
+  const personIdsInTeams = new Set(Object.keys(allPeopleTeamIdLists));
   console.log(Object.keys(allPeopleTeamIdLists).includes('67'));
 
   const arrayOfObjects =  [...incomingObjectList].sort((a, b) => {
